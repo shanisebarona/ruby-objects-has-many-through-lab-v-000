17f506525fc2a25aa7fb971aa62b8ca04ba56bbd
Shanise Barona <shanisebarona@gmail.com>
2018-09-01 19:32:05 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,11 +2,20 @@ require 'pry'
 
 class Artist 
   
-  attr_accessor :name
+  attr_accessor :name, :songs, :genres
   
   @@all = []
   
   def initialize(name)
     @name = name
     @@all << self
+    
+  end
+  
+  
+  def new_song
+    
+  end
+  
+  d
 end
