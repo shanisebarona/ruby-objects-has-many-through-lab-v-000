ac45e6ea3e0ec48a43ac74d4734b8dba52b15d82
Shanise Barona <shanisebarona@gmail.com>
2018-09-01 20:21:20 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,3 +1,18 @@
 class Patient
+  attr_accessor :name
   
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def new_appointment
+    
+  end
+  
+  def doctors
+    
+  end
 end
