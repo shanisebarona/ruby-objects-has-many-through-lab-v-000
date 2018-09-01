623ed7bfb97d5c201b6bdd6c47bd8ed2be4d205d
Shanise Barona <shanisebarona@gmail.com>
2018-09-01 20:11:24 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -9,4 +9,20 @@ class Genre
     @name = name
     @@all << self
   end
+  
+    def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def genres 
+    songs.map { |song| song.genre}
+  end
 end
